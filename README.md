ef4a06e20cbb3e207cd788613e4d103c01f350f1
Steve Polito <stevepolito@hey.com>
2022-02-25 16:37:37 -0500

    Initial commit
    Simply ran `rails new rails-server-side-analytics -d=postgresql` and updated the README.

---
+++ README.md
@@ -0,0 +1,22 @@
+# 📊 Rails Server Side Analytics
+
+This application will serve as an example of how to track analytics such as page views using Rails. Once complete, this application will be a companion to a blog post.
+
+## 📝 Outline
+
+- [Create visitor and track their events](https://github.com/thoughtbot/rails-server-side-analytics/commit/7d7126321670a966c677389d453dc7edd0301df8)
+- [Filter sensitive data from event records](https://github.com/thoughtbot/rails-server-side-analytics/commit/876fb6af0572c4e026ed94558d063b89989d69a8)
+- [Create event records in the background](https://github.com/thoughtbot/rails-server-side-analytics/commit/720ab23ff0b70a71b34b9e7eb9cbbebce09681fa)
+- [Allow a visitor to enable their session to be tracked](https://github.com/thoughtbot/rails-server-side-analytics/commit/f75195334b8390fea5158798fe5b02ef9b97f7a4)
+- [Create scopes to return analytics](https://github.com/thoughtbot/rails-server-side-analytics/commit/5726b14a6e30183040edfc9b6bf17d13c3daa3ab)
+- [Track conversions](https://github.com/thoughtbot/rails-server-side-analytics/commit/968a6f9d2e8d6712a4d0d566b3060187adf3d4b9)
+- [Render user analytics ](https://github.com/thoughtbot/rails-server-side-analytics/commit/2d57f6efe78c953c2eced44aaafceebfdae22ea4)
+
+## 🙏 Contributing
+
+If you'd like to open a PR please make sure the following items pass:
+
+```
+bin/rails test
+bundle exec standardrb
+```
---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_02_26_122953) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "path", null: false
+    t.string "method", null: false
+    t.string "params"
+    t.string "owner_type", null: false
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_type", "owner_id"], name: "index_events_on_owner"
+  end
+
+  create_table "visitors", force: :cascade do |t|
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
